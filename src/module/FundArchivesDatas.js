c49322c0d77b619c6f62c183bf8ae4750ed8446e
yifoo <wuhaoxcp@gmail.com>
2025-10-08 17:18:47 +0800

    增加基金持仓接口

---
+++ src/module/FundArchivesDatas.js
@@ -0,0 +1,46 @@
+const { request } = require('../utils/index.js');
+const cheerio = require('cheerio');
+
+/**
+ * 获取基金持仓数据
+ */
+module.exports = async (params = {}) => {
+  const url = 'https://fundf10.eastmoney.com/FundArchivesDatas.aspx';
+  const resp = await request(url, params);
+  const $ = cheerio.load(resp.split('"')[1])
+  const data = [];
+  $('table tbody tr').map(function() {
+    const $cells = $(this).find('td');
+    if(data.length>=parseInt(params.topline)||!$($cells[8]).text().trim()){
+      return false;
+    }
+    data.push({
+      rankNo: parseInt($($cells[0]).text().trim()),//占比排名
+      code: $($cells[1]).text().trim(),//股票代码
+      stockName: $($cells[2]).text().trim(),//股票名称
+      ratio: $($cells[6]).text().trim(),//占净值比例
+      shares: parseFloat($($cells[7]).text().trim()).toFixed(2),//持股数（万股）
+      money: parseFloat($($cells[8]).text().trim().replace(/,/g, ''))//持仓市值（万元）
+    });
+  })
+  let codeList = $("#gpdmList").text()
+  const resp2 = await request(`https://push2.eastmoney.com/api/qt/ulist.np/get?fltt=2&invt=2&fields=f2,f3,f12,f14`, {secids:codeList});
+  const fundsPriceLimit = resp2.data.diff
+  const result = new Map();
+    fundsPriceLimit.forEach(item => {
+      result.set(item.f12, item);
+    });
+  // 遍历 arr 数组，根据 fcode 匹配并添加 price 和 rate
+  data.forEach(item => {
+    const matchedItem = result.get(item.fcode);
+    if (matchedItem) {
+      item.price = matchedItem.f2;//最新价格
+      item.rate = matchedItem.f3;// 涨跌幅
+    }
+  });
+  
+  return {
+    code:200,
+    data,
+  }
+};
