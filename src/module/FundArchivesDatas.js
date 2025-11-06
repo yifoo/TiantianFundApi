d07358a8fec9ce95b1cb2055c6dc6d0a4b475820
yifoo <wuhaoxcp@gmail.com>
2025-11-06 23:50:02 +0800

    优化

--- src/module/FundArchivesDatas.js
+++ src/module/FundArchivesDatas.js
@@ -9,54 +9,60 @@ module.exports = async (params = {}) => {
   const resp = await request(url, params);
   const $ = cheerio.load(resp.split('"')[1])
   const data = [];
-  $('table tbody tr').map(function() {
+  $('table tbody tr').map(function () {
     const $cells = $(this).find('td');
-    if(data.length>=parseInt(params.topline)||!$($cells[8]).text().trim()){
+    if (data.length >= parseInt(params.topline) || !$($cells[8]).text().trim()) {
       return false;
     }
     data.push({
       rankNo: parseInt($($cells[0]).text().trim()),//占比排名
       code: $($cells[1]).text().trim(),//股票代码
       stockName: $($cells[2]).text().trim(),//股票名称
-      ratio: $($cells[6]).text().trim().slice(0,-1), //占净值比例
+      ratio: $($cells[6]).text().trim().slice(0, -1), //占净值比例
       shares: parseFloat($($cells[7]).text().trim()).toFixed(2),//持股数（万股）
       money: parseFloat($($cells[8]).text().trim().replace(/,/g, ''))//持仓市值（万元）
     });
   })
   let codeList = $("#gpdmList").text()
-  const resp2 = await request(`https://push2.eastmoney.com/api/qt/ulist.np/get?fltt=2&invt=2&fields=f2,f3,f12,f14`, {secids:codeList});
+  // console.log('codeList: ', codeList);
+  const resp2 = await request(`https://push2.eastmoney.com/api/qt/ulist.np/get?fltt=2&invt=2&fields=f2,f3,f12,f14`, { secids: codeList });
   const fundsPriceLimit = resp2.data.diff
+  // console.log('fundsPriceLimit: ', fundsPriceLimit);
   const result = new Map();
-    fundsPriceLimit.forEach(item => {
-      result.set(item.f12, item);
-    });
+  fundsPriceLimit.forEach(item => {
+    result.set(item.f12, item);
+  });
   codeList = codeList.split(",")
-  let stockTypeList=new Map();
-  for (let i of codeList){
-    if(i){
-    let resp = await request(`https://push2.eastmoney.com/api/qt/slist/get?fltt=1&invt=2&fields=f9,f12,f13,f14&pn=1&np=1&spt=1`, {secid:i});
-    stockTypeList.set(resp.data.diff[0].f12,{typeCode:resp.data.diff[1].f12,typeName:resp.data.diff[1].f14}) 
-    }
-  }
-  let sumRadio=0
+  // console.log('codeList: ', codeList);
+  // let stockTypeList = new Map();
+  // for (let i of codeList) {
+  //   if (i) {
+  //     let resp = await request(`https://push2.eastmoney.com/api/qt/slist/get?fltt=1&invt=2&fields=f9,f12,f13,f14&pn=1&np=1&spt=1`, { secid: i });
+  //     console.log('resp: ', resp.data.diff[0], resp.data.diff[1]);
+  //     stockTypeList.set(resp.data.diff[0].f12, { typeCode: resp.data.diff[1].f12, typeName: resp.data.diff[1].f14 })
+  //   }
+  // }
+  let sumRadio = 0
   // 遍历 arr 数组，根据 fcode 匹配并添加 price 和 rate
   data.forEach(item => {
     const matchedItem = result.get(item.fcode);
-    const matchedTypeItem = stockTypeList.get(item.code);
+    // const matchedTypeItem = stockTypeList.get(item.code);
     if (matchedItem) {
       item.price = matchedItem.f2;//最新价格
-      item.rate = matchedItem.f3;// 涨跌幅
+      item.rate = matchedItem.f3;// 持仓占比
+      item.typeCode = matchedItem.f12;
+      item.typeName = matchedItem.f14;
     }
-    if (matchedTypeItem) {
-      item.typeCode = matchedTypeItem.typeCode;
-      item.typeName = matchedTypeItem.typeName;
-    }
-    sumRadio+=parseFloat(item.ratio||0)
+    // if (matchedTypeItem) {
+    //   item.typeCode = matchedTypeItem.typeCode;
+    //   item.typeName = matchedTypeItem.typeName;
+    // }
+    sumRadio += parseFloat(item.ratio || 0)
   });
-  
+
   return {
-    code:200,
+    code: 200,
     data,
-    sumRadio:sumRadio.toFixed(2)
+    sumRadio: sumRadio.toFixed(2)
   }
 };
