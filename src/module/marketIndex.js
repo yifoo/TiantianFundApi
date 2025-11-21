674df0b4f26a1ae410094bdb86a4fd0b3ebe7330
yifoo <wuhaoxcp@gmail.com>
2025-11-21 12:05:40 +0800

    s

--- src/module/marketIndex.js
+++ src/module/marketIndex.js
@@ -7,7 +7,7 @@ module.exports = async (params = {}, ctx) => {
   const url = 'https://push2.eastmoney.com/api/qt/ulist.np/get';
   // params.secids = params.secids;
   //* 需指数对应代码："1.000001,0.399001"
-  params.fields = 'f2,f3,f4,f6,f12,f13,f14,f104,f105,f106'
+  params.fields = ['f2,f3,f4,f6,f12,f13,f14,f104,f105,f106']
   params.dpt = 'sc.wxdcxcx'
   params.fltt = 2
   // f6- 交易规模
@@ -20,19 +20,19 @@ module.exports = async (params = {}, ctx) => {
   // f2 - 最新价
   // f4 - 涨跌幅
   // f3 - 涨跌额
-  params = {
-    fields: ["f12%2Cf13%2Cf14%2Cf292%2Cf1%2Cf2%2Cf4%2Cf3%2Cf152"],
-    secids: "1.000001%2C0.399001%2C0.399006%2C100.HSI%2C100.HSCEI%2C100.HSAHP%2C100.DJIA%2C100.SPX%2C100.NDX",
-    dpt: "sc.wxdcxcx",
-    fltt: ["1"],
-    np: 1,
-    invt: 2,
-    fs: "i%3A100.SX5E%2Ci%3A100.FTSE%2Ci%3A100.MCX%2Ci%3A100.AXX%2Ci%3A100.FCHI%2Ci%3A100.GDAXI%2Ci%3A100.RTS%2Ci%3A100.IBEX%2Ci%3A100.PSI20%2Ci%3A100.OMXC20%2Ci%3A100.BFX%2Ci%3A100.AEX%2Ci%3A100.WIG%2Ci%3A100.OMXSPI%2Ci%3A100.SSMI%2Ci%3A100.HEX%2Ci%3A100.OSEBX%2Ci%3A100.ATX%2Ci%3A100.MIB%2Ci%3A100.ASE%2Ci%3A100.ICEXI%2Ci%3A100.PX%2Ci%3A100.ISEQ",
-    pn: 1,
-    pz: 100,
-    po: 1,
-    dect: 1,
-  }
+  // params = {
+  //   fields: ["f12%2Cf13%2Cf14%2Cf292%2Cf1%2Cf2%2Cf4%2Cf3%2Cf152"],
+  //   secids: "1.000001%2C0.399001%2C0.399006%2C100.HSI%2C100.HSCEI%2C100.HSAHP%2C100.DJIA%2C100.SPX%2C100.NDX",
+  //   dpt: "sc.wxdcxcx",
+  //   fltt: ["1"],
+  //   np: 1,
+  //   invt: 2,
+  //   fs: "i%3A100.SX5E%2Ci%3A100.FTSE%2Ci%3A100.MCX%2Ci%3A100.AXX%2Ci%3A100.FCHI%2Ci%3A100.GDAXI%2Ci%3A100.RTS%2Ci%3A100.IBEX%2Ci%3A100.PSI20%2Ci%3A100.OMXC20%2Ci%3A100.BFX%2Ci%3A100.AEX%2Ci%3A100.WIG%2Ci%3A100.OMXSPI%2Ci%3A100.SSMI%2Ci%3A100.HEX%2Ci%3A100.OSEBX%2Ci%3A100.ATX%2Ci%3A100.MIB%2Ci%3A100.ASE%2Ci%3A100.ICEXI%2Ci%3A100.PX%2Ci%3A100.ISEQ",
+  //   pn: 1,
+  //   pz: 100,
+  //   po: 1,
+  //   dect: 1,
+  // }
   let header = {
     "Accept": "*/*",
     "Accept-Encoding": "gzip, deflate, br, zstd",
@@ -50,7 +50,8 @@ module.exports = async (params = {}, ctx) => {
     "sec-ch-ua-platform": "macOS"
   }
   try {
-    let res = await get(url, params, header);
+    console.log('params: ', params);
+    let res = await get(url, {}, header);
     return {
       code: 200,
       data: res || {}
