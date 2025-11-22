f411ec67dc064b656dd4d598b4ab5766dc58e1b2
yifoo <wuhaoxcp@gmail.com>
2025-11-22 22:35:24 +0800

    121

--- src/module/sse.js
+++ src/module/sse.js
@@ -13,7 +13,7 @@ module.exports = async (params = {}, ctx) => {
     ctx.set('Cache-Control', 'no-cache');
     ctx.set('Connection', 'keep-alive');
 
-    let res = await sse('/api/qt/ulist/sse', {
+    let res = await sse('https://push2.eastmoney.com/api/qt/ulist/sse', {
         "fltt": "2",
         "secids": params.secids,
         "fields": params.fields,
@@ -21,7 +21,6 @@ module.exports = async (params = {}, ctx) => {
         "pn": "1",
         "pz": "100"
     });
-    console.log('res: ', res);
     return res
 };
 
